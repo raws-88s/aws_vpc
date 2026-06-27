3d9d02e3caf15174b84fb1c33dc36ffbb11fd853
rajesh maddula <rajesh.maddula21@gmail.com>
2026-06-27 11:49:01 +0530

    action

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
-  enable_dns_hostnames = yes
+  enable_dns_hostnames = true
 
 #roboshop-dev
   tags = local.vpc_final_tags
