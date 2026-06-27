cb883d6faf9c64d685ab16b3966d49a2575e5aec
rajesh maddula <rajesh.maddula21@gmail.com>
2026-06-27 10:50:50 +0530

    action

---
+++ locals.tf
@@ -0,0 +1,12 @@
+locals {
+  common_tags ={
+    Project = var.project
+    Environment = var.environment
+    terraform = true
+  }
+  vpc_final_tags = merge(
+    local.common_tags,
+    Name = "${var.project}-${var.environment}"
+  ),
+  var.vpc_tags
+}
---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+  enable_dns_hostnames = yes
+
+#roboshop-dev
+  tags = local.igw_final_tags
+}
