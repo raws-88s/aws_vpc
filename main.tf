c70f8513bce77a2338fccda3adb512ccd9d0c04e
rajesh maddula <rajesh.maddula21@gmail.com>
2026-06-28 08:38:34 +0530

    action

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
-  availability_zones = local.az_zones[count.index]
+  availability_zone = local.az_zones[count.index]
   map_public_ip_on_launch = true
   
   #roboshop-dev-public-us-east-1a
